b7fd84d2accde28c49e7bcd2eaf9fedc1b218b34
Luane Souza <luanesouza@Luanes-MBP.fios-router.home>
2019-10-31 23:10:38 -0400

    initial commit

---
+++ src/components/WomenInequality.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const WomenInequality = () => {
+  return(
+    <section>
+      <h1></h1>
+      <ul>
+        <li>
+        Only 25% of IT jobs are held by women.
+        </li>
+
+      </ul>
+    </section>
+  )
+}
+export default WomenInequality;
