26a82c2b62385d9c777559e8c61b64e1086836ea
luanesouza <luanesouza411@gmail.com>
2020-02-20 10:43:31 -0500

    changes title for component: AgeGaps

--- src/components/AgeGapFacts.js
+++ src/components/AgeGapFacts.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AgeGapFacts = () => {
   return (
     <section>
-      <h1>How old are our problems?</h1>
+      <h1>What is the age of our problems?</h1>
 
     <footer>
       <p> Numbers taken from <a href='http://blog.indeed.com/2017/10/19/tech-ageism-report/'>indeed.com </a> </p>
